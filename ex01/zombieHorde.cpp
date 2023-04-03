683697bee853516f42cb78b0bf8834192ee3ae97
Jean MANET <jmanet@Jeans-MacBook-Pro.local>
2023-04-03 09:46:20 +0200

    Changes for +98 compatibility

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,11 +6,18 @@
 /*   By: jmanet <jmanet@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 18:41:02 by jmanet            #+#    #+#             */
-/*   Updated: 2023/03/31 20:44:36 by jmanet           ###   ########.fr       */
+/*   Updated: 2023/04/03 09:43:40 by jmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+#include <sstream>
+
+std::string	to_str(int number) {
+    std::ostringstream ss;
+    ss << number;
+    return ss.str();
+}
 
 Zombie* zombieHorde( int N, std::string name ) {
 	int			i = 0;
@@ -19,7 +26,7 @@ Zombie* zombieHorde( int N, std::string name ) {
 
 	while (i < N)
 	{
-		current_name = name + std::to_string(i + 1);
+		current_name = name + to_str(i + 1);
 		zombies[i].setName(current_name);
 		std::cout << "Un zombie a ete baptise " << current_name << std::endl;
 		i++;
